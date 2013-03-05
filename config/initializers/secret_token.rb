7a86c0a8280654127ab789d0b57966ee12bf33af
Jen <jenstrumental@gmail.com>
2013-03-04 17:43:45 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scratch::Application.config.secret_token = '159f92baa1eb4b8d4357e8ea386837bfe0d8caa1cb9717252ff8887a35040b8e635b2d506950c0567c7d62610eae01fd5dac5fc3f65819be4259d329c00e1437'
